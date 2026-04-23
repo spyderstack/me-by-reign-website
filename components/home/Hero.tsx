3f56aff5c1ce8b5d1f365d5ce129d384899644a5
j-a-man <jaylinman4@gmail.com>
2026-04-23 09:36:04 -0400

    new her

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -18,6 +18,7 @@ export function Hero() {
           //src="https://images.unsplash.com/photo-1517487881594-2787fef5ebf7?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           //src="/images/heroimage.png"
           src="/images/image2.png"
+          //src="https://unsplash.com/photos/white-ceramic-round-plate-beside-white-ceramic-teacup-on-brown-wooden-table-a-LwvEw6TiM"
           alt="ME byReign — Artisan handmade skincare and home decor"
           fill
           sizes="100vw"
